0607831a012804fa56ad6d2931d20a913164ff47
leleliu008 <792793182@qq.com>
2015-05-22 17:41:38 +0800

    增加PCM转WAV的算法

--- src/com/leleliu008/newton/base/audio/AudioManager.java
+++ src/com/leleliu008/newton/base/audio/AudioManager.java
@@ -1,5 +1,11 @@
 package com.leleliu008.newton.base.audio;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.leleliu008.newton.base.DebugLog;
 
 /**
  * 音频处理
@@ -9,16 +15,19 @@ package com.leleliu008.newton.base.audio;
  */
 public final class AudioManager {
 
+	private static final String TAG = AudioManager.class.getSimpleName();
+
 	private static final class InstanceHolder {
 		private static AudioManager instance = new AudioManager();
 	}
-	
+
 	public static AudioManager getInstance() {
 		return InstanceHolder.instance;
 	}
-	
+
 	/**
 	 * 16位的WAV文件头，插入这些信息就可以得到可以播放的文件。
+	 * 
 	 * @param totalAudioLen
 	 * @param totalDataLen
 	 * @param longSampleRate
@@ -26,8 +35,8 @@ public final class AudioManager {
 	 * @param byteRate
 	 * @return
 	 */
-	public byte[] getWAVHeader(long totalAudioLen, long totalDataLen, long longSampleRate,
-			int channels, long byteRate) {
+	public byte[] getWAVHeader(long totalAudioLen, long totalDataLen,
+			long longSampleRate, int channels, long byteRate) {
 		byte[] header = new byte[44];
 		header[0] = 'R'; // RIFF/WAVE header
 		header[1] = 'I';
@@ -75,5 +84,54 @@ public final class AudioManager {
 		header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
 
 		return header;
+	}
+
+	/**
+	 * 将PCM文件转换成WAV文件
+	 * @param pcmFile          PCM文件
+	 * @param wavFile          WAV文件
+	 * @param longSampleRate   采样率
+	 * @param channels         通道数量
+	 */
+	private void convertPCM2WAV(File pcmFile, File wavFile, long sampleRate, int channels) {
+		FileInputStream in = null;
+		FileOutputStream out = null;
+		
+		try {
+			in = new FileInputStream(pcmFile);
+			out = new FileOutputStream(wavFile);
+
+			long totalAudioLen = in.getChannel().size();
+			long totalDataLen = totalAudioLen + 36;
+
+			long byteRate = 16 * sampleRate * channels / 8;
+			
+			// 写入头
+			out.write(getWAVHeader(totalAudioLen, totalDataLen, sampleRate,
+					channels, byteRate));
+
+			byte[] data = new byte[1024];
+			// 写入PCM数据
+			while (in.read(data) != -1) {
+				out.write(data);
+			}
+		} catch (Exception e) {
+			DebugLog.e(TAG, "convertPCM2WAV()", e);
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					DebugLog.e(TAG, "convertPCM2WAV()", e);
+				}
+			}
+			if (out != null) {
+				try {
+					out.close();
+				} catch (IOException e) {
+					DebugLog.e(TAG, "convertPCM2WAV()", e);
+				}
+			}
+		}
 	}
 }
